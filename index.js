0a3994aae0f524ae55d4ed42b6bc8704412be9fe
Bhavesh <bhaveshjat9950@gmail.com>
2021-07-31 10:16:36 +0530

    boilerplate for basic Node.js and express.js app

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-//This is index.js
+//Default exports
+const express = require("express");
+
+//importing files
+const routes = require("./router/routes.js");
+
+//initialising server
+const app = express();
+
+//middlewares
+app.use(routes);
+
+//port setting
+const PORT = process.env.PORT || 5000;
+
+// starting the server
+app.listen(PORT, () => {
+	console.log(`App is running on PORT ${PORT}`);
+});
